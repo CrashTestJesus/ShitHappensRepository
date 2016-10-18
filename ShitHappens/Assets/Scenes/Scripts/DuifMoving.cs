90de83ac779ab995398def206164bd4f255a115a
zeldaddicted <cynthiavanhees@hotmail.com>
2016-10-18 15:23:40 +0200

    Test

---
+++ ShitHappens/Assets/Scenes/Scripts/DuifMoving.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DuifMoving : MonoBehaviour {
+
+    float movingSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * movingSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * movingSpeed * Time.deltaTime);
+        }
+    }
+}
