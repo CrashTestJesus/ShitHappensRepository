b1e717ce413b6ca1c06d4191cd38612218955f03
zeldaddicted <cynthiavanhees@hotmail.com>
2016-10-19 20:14:12 +0200

    Begin Random human spawn

---
+++ ShitHappens/Assets/Scripts/HumanSpeeds.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanSpeeds : MonoBehaviour {
+
+    float movingSpeed = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector2.left * movingSpeed * Time.deltaTime);
+	}
+}
