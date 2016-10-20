0dd36bf8d76ac34c1244e75c7406f7d2dccde935
CrashTestJesus <l.hulsmans@sintlucasedu.nl>
2016-10-20 15:35:42 +0200

    added some scenes and did some other stuff

---
+++ ShitHappens/Assets/Scripts/MenuNav.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuNav : MonoBehaviour {
+
+    public float delay;
+
+    int index = 1;
+
+    bool canMove = true;
+    bool bottomRow;
+	
+	void Update () {
+        float xMove = Input.GetAxis("Horizontal");
+        float yMove = Input.GetAxis("Vertical"); 
+
+        if(xMove > 0 && canMove && index >= 1 && index <= 2)
+        {
+            canMove = false;
+            index++;
+            StartCoroutine(SelectDelay());
+        }
+        if (xMove < 0 && canMove && index >= 2 && index <= 3)
+        {
+            canMove = false;
+            index--;
+            StartCoroutine(SelectDelay());
+        }
+
+    }
+    IEnumerator SelectDelay()
+    {
+        yield return new WaitForSeconds(delay);
+
+        canMove = true;
+    }
+}
