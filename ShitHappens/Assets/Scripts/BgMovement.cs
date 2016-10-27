10f3a582c6076aebfa5a78468eb7c6c81f866699
CrashTestJesus <l.hulsmans@sintlucasedu.nl>
2016-10-27 12:39:53 +0200

    bgAdded

---
+++ ShitHappens/Assets/Scripts/BgMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BgMovement : MonoBehaviour {
+
+
+    public float speed;
+    public float xMin;
+    public float spawnX;
+
+	void Update () {
+        transform.position -= new Vector3(speed, 0, 0) * Time.deltaTime;
+        if (transform.position.x < xMin)
+        {
+            transform.position = new Vector3(spawnX, transform.position.y, transform.position.z);
+        }
+	}
+}
