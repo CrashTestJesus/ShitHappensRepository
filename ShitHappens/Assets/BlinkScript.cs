b3691902b33027ab7435f861960e0ea181ca356b
CrashTestJesus <l.hulsmans@sintlucasedu.nl>
2016-11-09 09:11:18 +0100

    yesterdays stuff, hurray for trump

---
+++ ShitHappens/Assets/BlinkScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class BlinkScript : MonoBehaviour {
+
+    Animator animator;
+    bool runTimer;
+
+    float currTime;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (runTimer)
+        {
+            currTime -= Time.deltaTime;
+            if(currTime <= 0)
+            {
+                animator.SetBool("blink", true);
+            }
+        }
+    }
+    void SetUpTimer()
+    {
+        currTime = Random.Range(2, 4);
+
+    }
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(0.1f);
+        animator.SetBool("blink", false);
+    }
+}
+
